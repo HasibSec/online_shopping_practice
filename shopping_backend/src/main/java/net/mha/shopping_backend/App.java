e3d7d84d422ac9db9dfceb3efff2a3a8f9f8386b
Md. Hasibul Alam <hasibsjc87@gmail.com>
2017-11-19 22:46:25 +0600

    My Project Setup Completed Successfully

---
+++ shopping_backend/src/main/java/net/mha/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package net.mha.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
